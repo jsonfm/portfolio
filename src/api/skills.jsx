8485529379147d8b35dc2dd381d9a2262872ce73
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-19 23:46:36 -0500

    🎨 [UPDATE] assets

--- src/api/skills.jsx
+++ src/api/skills.jsx
@@ -1,34 +1,43 @@
+import reactIcon from "@/assets/react.webp";
+import pythonIcon from "@/assets/python.webp";
+import javascriptIcon from "@/assets/javascript.webp";
+import typescriptIcon from "@/assets/typescript.webp";
+import tailwindIcon from "@/assets/tailwind.webp";
+import nodeIcon from "@/assets/node.webp";
+import postgresIcon from "@/assets/postgres.webp";
+import gitIcon from "@/assets/git.webp";
+
 export const skills = [
     {
         name: "React JS",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+        image: reactIcon
     },
     {
         name: "Python",
-        image: "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png",
+        image: pythonIcon,
     },
     {
         name: "Javascript",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+        image: javascriptIcon,
     },
     {
         name: "Typescript",
-        image: "https://www.tutorialsteacher.com/Content/images/home/typescript.svg",
+        image: typescriptIcon,
     },
     {
         name: "TailwindCSS",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png",
+        image: tailwindIcon,
     },
     {
         name: "Node JS",
-        image: "https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png"
+        image: nodeIcon,
     },
     {
         name: "Postgres",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"
+        image: postgresIcon,
     },
     {
         name: "Git",
-        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+        image: gitIcon,
     }
 ];
