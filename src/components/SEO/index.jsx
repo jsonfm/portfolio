a04f794585ca6d5d5ad3ba287c81e764bf63f3d5
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-20 19:31:52 -0500

    🔥 [UPDATE] SEO

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -5,12 +5,12 @@ export const SEO = () => {
     return (
         <Helmet>
             <title>Portfolio</title>
-            <meta property="description" content="This is my personal portfolio, here is my profesional experience and my skills." />
+            <meta property="description" content="This is my personal portfolio, here is my professional experience and my skills." />
             <link rel="canonical" href="https://jsonfm.github.io/portfolio/" />
             <meta name="url" content="https://jsonfm.github.io/portfolio/"/>
             <meta name="type" property="og:type" content="website" />
             <meta name="title" property="og:title" content="Portfolio" />
-            <meta name="description" property="og:description" content="This is my personal portfolio, here is my profesional experience and my skills." />
+            <meta name="description" property="og:description" content="This is my personal portfolio, here is my professional experience and my skills." />
             <meta name="image" property="og:image" content="https://github.com/jsonfm/portfolio/blob/master/docs/images/portfolio.png?raw=true"/>
         </Helmet>
     )
